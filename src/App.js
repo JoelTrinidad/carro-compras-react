8d7b1350364fe253113025c65bee2d86192ea7d5
Joel Trinidad <jth_2929@hotmail.com>
2022-09-26 12:53:46 -0500

    feat: initial state and images loaded

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    productos: [
+      {name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
+      {name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg'},
+      {name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},
+    ]
+  }
   render() {
     return (
       <div>
