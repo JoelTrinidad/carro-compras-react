af70cf71b130f7a850b6743b7fb4fd331203ada2
Joel Trinidad <jth_2929@hotmail.com>
2022-09-27 15:34:18 -0500

    feat: carro added to state and agregarAlCarro function created

--- src/App.js
+++ src/App.js
@@ -11,7 +11,25 @@ class App extends Component {
       { name: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
       { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
     ],
+    carro: [],
   };
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    if (carro.find((x) => x.name === producto.name)) {
+      const newCarro = carro.map((x) =>
+        x.name === producto.name ? { ...x, cantidad: x.cantidad + 1 } : x
+      );
+      return this.setState({ carro: newCarro });
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      }),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -19,7 +37,7 @@ class App extends Component {
         <Layout>
           <Title />
           <Productos
-            agregarAlCarro={() => console.log("No hace nada")}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
